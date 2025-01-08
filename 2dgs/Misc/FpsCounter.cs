791de378bba3a0236cea4c847a68ba12c2e0e78b
Alex McCullough <alexmccullough@outlook.com>
2025-01-08 19:17:17 +0000

    chore: changed fps counter to medium font

--- 2dgs/Misc/FpsCounter.cs
+++ 2dgs/Misc/FpsCounter.cs
@@ -42,7 +42,7 @@ public class FpsCounter
         {
             spriteBatch.Begin();
             string fpsText = $"FPS: {_fps}";
-            _fontManager.BoldFont(FontSize).
+            _fontManager.MediumFont(FontSize).
                 DrawText(spriteBatch, fpsText, new Vector2(10, 10), Color.White);
             spriteBatch.End();
         }
