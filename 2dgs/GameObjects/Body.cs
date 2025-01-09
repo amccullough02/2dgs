8d4b7566a54b24982edabc2ff6927005c59a816d
Alex McCullough <alexmccullough@outlook.com>
2025-01-09 15:01:10 +0000

    refactor: cleaned up body.cs

--- 2dgs/GameObjects/Body.cs
+++ 2dgs/GameObjects/Body.cs
@@ -8,8 +8,6 @@ namespace _2dgs;
 
 public class Body
 {
-    private Texture2D _texture;
-    private Texture2D _orbitTexture;
     public Vector2 _position;
     private Vector2 _velocity;
     private string _name;
@@ -19,6 +17,7 @@ public class Body
     private float _displayRadius;
     private const int FontSize = 20;
     private FontManager _fontManager;
+    private TextureManager _textureManager;
 
     public Body(string name, Vector2 position, Vector2 velocity, float mass, float displayRadius, TextureManager textureManager)
     {
@@ -29,8 +28,7 @@ public class Body
         _displayRadius = displayRadius;
         _orbit_trail = new List<Vector2>();
         _fontManager = new FontManager();
-        _texture = textureManager.BodyTexture;
-        _orbitTexture = textureManager.OrbitTexture;
+        _textureManager = textureManager;
     }
 
     private Vector2 CalculateGravity(Body otherBody)
@@ -69,99 +67,105 @@ public class Body
         }
     }
 
-    private void DrawOrbit(SpriteBatch spriteBatch, Vector2 start, Vector2 end, Color color, float thickness)
-    {
-        Vector2 direction = end - start;
-        float length = direction.Length();
-        float angle = (float)Math.Atan2(direction.Y, direction.X);
-        
-        spriteBatch.Draw(_orbitTexture,
-            start,
-            null,
-            color,
-            angle,
-            Vector2.Zero,
-            new Vector2(length,
-                thickness),
-            SpriteEffects.None,
-            0f);
-    }
-
-    public void Draw(SpriteBatch spriteBatch, SimulationData simData)
+    private void DrawOrbit(SpriteBatch spriteBatch, SimulationData simData, Color color, float thickness)
     {
         if (_orbit_trail.Count > 1 && simData.ToggleTrails)
         {
 
             int trailLength = Math.Min(simData.TrailLength, _orbit_trail.Count);
-
             for (int i = _orbit_trail.Count - trailLength; i < _orbit_trail.Count - 1; i++)
             {
-                DrawOrbit(spriteBatch, _orbit_trail[i], _orbit_trail[i + 1], Color.White, 2f);
+                Vector2 direction = _orbit_trail[i] - _orbit_trail[i + 1];
+                float length = direction.Length();
+                float angle = (float)Math.Atan2(direction.Y, direction.X);
+
+                spriteBatch.Draw(_textureManager.OrbitTexture,
+                    _orbit_trail[i + 1],
+                    null,
+                    color,
+                    angle,
+                    Vector2.Zero,
+                    new Vector2(length,
+                        thickness),
+                    SpriteEffects.None,
+                    0f);
             }
         }
+    }
 
-        if (_texture == null)
+    private void DrawBody(SpriteBatch spriteBatch)
+    {
+        if (_textureManager.BodyTexture == null)
         {
             throw new InvalidOperationException("Body has no texture!");
         }
 
-        spriteBatch.Draw(_texture,
+        spriteBatch.Draw(_textureManager.BodyTexture,
             _position,
             null,
             Color.White,
             0f,
-            new Vector2(_texture.Width / 2, _texture.Height / 2),
+            new Vector2(_textureManager.BodyTexture.Width / 2, _textureManager.BodyTexture.Height / 2),
             new Vector2(_displayRadius, _displayRadius),
             SpriteEffects.None,
             0f);
+    }
 
-        if (simData.ToggleNames)
+    private void DrawNames(SimulationData simData, SpriteBatch spriteBatch)
+    {
+        if (!simData.ToggleNames) return;
+        switch (simData.Position)
+        {
+            case Position.Left:
+                _fontManager.LightFont(FontSize)
+                    .DrawText(spriteBatch,
+                        _name,
+                        _position +
+                        new Vector2((_displayRadius * 600) + 5,
+                            -10f),
+                        Color.White);
+                break;
+            case Position.Right:
+                _fontManager.LightFont(FontSize)
+                    .DrawText(spriteBatch,
+                        _name,
+                        _position +
+                        new Vector2((-_displayRadius * 600) - 5 - (FontSize * _name.Length / 1.5f),
+                            -10f),
+                        Color.White);
+                break;
+            case Position.Bottom:
+                _fontManager.LightFont(FontSize)
+                    .DrawText(spriteBatch,
+                        _name,
+                        _position +
+                        new Vector2(-FontSize * _name.Length / 3, _displayRadius * 600),
+                        Color.White);
+                break;
+            case Position.Top:
+                _fontManager.LightFont(FontSize)
+                    .DrawText(spriteBatch,
+                        _name,
+                        _position +
+                        new Vector2(-FontSize * _name.Length / 3, -(_displayRadius * 600) - FontSize),
+                        Color.White);
+                break;
+            default:
+                _fontManager.LightFont(FontSize)
+                    .DrawText(spriteBatch,
+                        "Test Name",
+                        _position +
+                        new Vector2(_displayRadius * 600,
+                            -10f),
+                        Color.White);
+                break;
+        }
+    }
 
-            switch (simData.Position)
-            {
-                case Position.Left:
-                    _fontManager.LightFont(FontSize)
-                        .DrawText(spriteBatch,
-                            _name,
-                            _position +
-                            new Vector2((_displayRadius * 600) + 5,
-                                -10f),
-                            Color.White);
-                    break;
-                case Position.Right:
-                    _fontManager.LightFont(FontSize)
-                        .DrawText(spriteBatch,
-                            _name,
-                            _position +
-                            new Vector2((-_displayRadius * 600) - 5 - (FontSize * _name.Length / 1.5f),
-                                -10f),
-                            Color.White);
-                    break;
-                case Position.Bottom:
-                    _fontManager.LightFont(FontSize)
-                        .DrawText(spriteBatch,
-                            _name,
-                            _position +
-                            new Vector2(-FontSize * _name.Length / 3, _displayRadius * 600),
-                            Color.White);
-                    break;
-                case Position.Top:
-                    _fontManager.LightFont(FontSize)
-                        .DrawText(spriteBatch,
-                            _name,
-                            _position +
-                            new Vector2(-FontSize * _name.Length / 3, -(_displayRadius * 600) - FontSize),
-                            Color.White);
-                    break;
-                default:
-                    _fontManager.LightFont(FontSize)
-                        .DrawText(spriteBatch,
-                            "Test Name",
-                            _position +
-                            new Vector2(_displayRadius * 600,
-                                -10f),
-                            Color.White);
-                    break;
-            }
+    public void Draw(SpriteBatch spriteBatch, SimulationData simData)
+    {
+        DrawOrbit(spriteBatch, simData, Color.White, 2f);
+        DrawBody(spriteBatch);
+        DrawNames(simData, spriteBatch);
     }
 }
