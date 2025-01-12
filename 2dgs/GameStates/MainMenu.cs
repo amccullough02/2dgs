95c0b96d3ca5f93ad2dfdb6f542ff4accc2d04b6
Alex McCullough <alexmccullough@outlook.com>
2025-01-12 14:04:42 +0000

    refactor: created MainMenuUi.cs to contain MainMenu.cs UI logic

--- 2dgs/GameStates/MainMenu.cs
+++ 2dgs/GameStates/MainMenu.cs
@@ -1,131 +1,21 @@
-﻿using System;
-using System.IO;
-using FontStashSharp;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Myra;
-using Myra.Graphics2D.UI;
 
 namespace _2dgs;
 
 public class MainMenu : GameState
 {
-    private Desktop _desktop;
-    private FontManager _fontManager;
-    private Game game;
+    private readonly MainMenuUi _mainMenuUi;
 
     public MainMenu(Game game)
     {
-        this.game = game;
-        
-        _fontManager = new FontManager();
-
-        MyraEnvironment.Game = this.game;
-
-        var grid = new Grid
-        {
-            RowSpacing = 10,
-            HorizontalAlignment = HorizontalAlignment.Center,
-            VerticalAlignment = VerticalAlignment.Center,
-        };
-        
-        grid.ColumnsProportions.Add(new Proportion(ProportionType.Auto));
-        grid.RowsProportions.Add(new Proportion(ProportionType.Auto)); // TITLE
-        grid.RowsProportions.Add(new Proportion(ProportionType.Auto)); // SIMS
-        grid.RowsProportions.Add(new Proportion(ProportionType.Auto)); // SETTINGS
-        grid.RowsProportions.Add(new Proportion(ProportionType.Auto)); // QUIT
-
-        var title = new Label
-        {
-            Id = "title",
-            Text = "2DGS",
-            Font = _fontManager.LightFont(70),
-            HorizontalAlignment = HorizontalAlignment.Center
-        };
-        
-        Grid.SetRow(title, 0);
-        grid.Widgets.Add(title);
-        
-        var simulationMenu = new Button
-        {
-            Width=300,
-            Height=75,
-            Content = new Label
-            {
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center,
-                Text = "Simulations",
-                Font = _fontManager.LightFont(20),
-            }
-        };
-        
-        Grid.SetRow(simulationMenu, 1);
-
-        simulationMenu.Click += (s, a) =>
-        {
-            Console.WriteLine("DEBUG: Navigating to simulation menu...");
-            this.game.GameStateManager.ChangeState(new SimulationMenu(this.game));
-        };
-
-        grid.Widgets.Add(simulationMenu);
-        
-        var settingsMenu = new Button
-        {
-            Width=300,
-            Height=75,
-            Content = new Label
-            {
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center,
-                Text = "Settings",
-                Font = _fontManager.LightFont(20),
-            }
-        };
-        
-        Grid.SetRow(settingsMenu, 2);
-
-        settingsMenu.Click += (s, a) =>
-        {
-            Console.WriteLine("DEBUG: Navigating to settings menu...");
-            this.game.GameStateManager.ChangeState(new SettingsMenu(this.game));
-        };
-
-        grid.Widgets.Add(settingsMenu);
-        
-        var quitButton = new Button
-        {
-            Width=300,
-            Height=75,
-            Content = new Label
-            {
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center,
-                Text = "Quit",
-                Font = _fontManager.LightFont(20),
-            }
-        };
-        
-        Grid.SetRow(quitButton, 3);
-
-        quitButton.Click += (s, a) =>
-        {
-            MyraEnvironment.Game.Exit();
-        };
-
-        grid.Widgets.Add(quitButton);
-        
-        _desktop = new Desktop();
-        _desktop.Root = grid;
+        _mainMenuUi = new MainMenuUi(game);
     }
     
-    public override void Update(GameTime gameTime)
-    {
-        // Left intentionally blank.
-    }
+    public override void Update(GameTime gameTime) {}
 
     public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
-        _desktop.Render();
+        _mainMenuUi.Draw();
     }
 }
---
+++ 2dgs/GameStates/MainMenuUi.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Myra;
+using Myra.Graphics2D;
+using Myra.Graphics2D.UI;
+
+namespace _2dgs;
+
+public class MainMenuUi
+{
+    private Desktop _desktop;
+
+    public MainMenuUi(Game game)
+    {
+        MyraEnvironment.Game = game;
+
+        var rootContainer = new Panel();
+        rootContainer.Widgets.Add(CreateMainMenu(game));
+        
+        _desktop = new Desktop();
+        _desktop.Root = rootContainer;
+    }
+
+    private VerticalStackPanel CreateMainMenu(Game game)
+    {
+        var mainMenuStackPanel = UiComponents.CreateVerticalStackPanel(8, HorizontalAlignment.Center,
+            VerticalAlignment.Center, new Thickness(UiConstants.DefaultMargin));
+        
+        var menuLabel = UiComponents.CreateStyledLabel("2DGS");
+
+        var simulationMenuButton = UiComponents.CreateButton("Simulation Menu");
+        simulationMenuButton.Click += (s, e) =>
+        {
+            Console.WriteLine("DEBUG: Navigating to simulation menu...");
+            game.GameStateManager.ChangeState(new SimulationMenu(game));
+        };
+        
+        var settingsMenuButton = UiComponents.CreateButton("Settings");
+        settingsMenuButton.Click += (s, a) =>
+        {
+            Console.WriteLine("DEBUG: Navigating to settings menu...");
+            game.GameStateManager.ChangeState(new SettingsMenu(game));
+        };
+        
+        var quitButton = UiComponents.CreateButton("Quit");
+        quitButton.Click += (s, a) =>
+        {
+            MyraEnvironment.Game.Exit();
+        };
+        
+        mainMenuStackPanel.Widgets.Add(menuLabel);
+        mainMenuStackPanel.Widgets.Add(simulationMenuButton);
+        mainMenuStackPanel.Widgets.Add(settingsMenuButton);
+        mainMenuStackPanel.Widgets.Add(quitButton);
+        
+        return mainMenuStackPanel;
+    }
+
+    public void Draw()
+    {
+        _desktop.Render();
+    }
+}
