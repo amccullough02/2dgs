57590012fd4ff17506d48bb431022cd463193c8f
Alex McCullough <alexmccullough@outlook.com>
2025-01-15 20:26:07 +0000

    fix: reference to deleted content caused crash

--- 2dgs/Graphics/TextureManager.cs
+++ 2dgs/Graphics/TextureManager.cs
@@ -8,12 +8,10 @@ public class TextureManager
 {
     public Texture2D BodyTexture { get; private set; }
     public Texture2D OrbitTexture { get; private set; }
-    public Texture2D SelectorTexture { get; private set; }
 
     public TextureManager(ContentManager content, GraphicsDevice graphicsDevice)
     {
         BodyTexture = content.Load<Texture2D>("blank_circle");
-        SelectorTexture = content.Load<Texture2D>("selector");
         OrbitTexture = new Texture2D(graphicsDevice, 1, 1);
         OrbitTexture.SetData([Color.White]);
     }
