afebf1e5d7316d9920490927e09dd594c85b03a1
Alex McCullough <alexmccullough@outlook.com>
2025-02-26 13:22:34 +0000

    docs: documented Scene class

--- 2dgs/Scenes/Scene.cs
+++ 2dgs/Scenes/Scene.cs
@@ -3,8 +3,20 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace _2dgs;
 
+/// <summary>
+/// An abstract class that provides a standardized implementation for 2DGS scenes.
+/// </summary>
 public abstract class Scene
 {
+    /// <summary>
+    /// An abstract method for updating the contents of a scene.
+    /// </summary>
+    /// <param name="gameTime">Access for MonoGame's GameTime class.</param>
     public abstract void Update(GameTime gameTime);
+    /// <summary>
+    /// An abstract method for drawing the contents of a scene.
+    /// </summary>
+    /// <param name="gameTime">Access for MonoGame's GameTime class.</param>
+    /// <param name="spriteBatch">Access for MonoGame's SpriteBatch class.</param>
     public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
 }
