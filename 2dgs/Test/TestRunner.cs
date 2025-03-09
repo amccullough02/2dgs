db0e2da902801ef49a3b6dac3bba06f127e1b561
Alex McCullough <alexmccullough@outlook.com>
2025-03-09 17:28:53 +0000

    docs: TestRunner

--- 2dgs/Test/TestRunner.cs
+++ 2dgs/Test/TestRunner.cs
@@ -7,12 +7,29 @@ using Myra.Graphics2D.UI;
 
 namespace _2dgs;
 
+/// <summary>
+/// A class used for testing the 2DGS application.
+/// </summary>
 public static class TestRunner
 {
+    /// <summary>
+    /// A list for test results.
+    /// </summary>
     private static List<string> _results = [];
+    /// <summary>
+    /// Marks when the testing process is complete, called in the final test.
+    /// </summary>
     private static bool _testingComplete;
+    /// <summary>
+    /// The path for the test results file.
+    /// </summary>
     private const string ResultsPath = "../../../savedata/test/results.txt";
 
+    /// <summary>
+    /// A method to test if the application name is as expected.
+    /// </summary>
+    /// <param name="actualName">The actual name of the application.</param>
+    /// <param name="expectedName">The name the application should actually be.</param>
     public static void AssertApplicationName(string actualName, string expectedName)
     {
         if (actualName != expectedName)
@@ -29,6 +46,11 @@ public static class TestRunner
         }   
     }
 
+    /// <summary>
+    /// A method to test if the application resolution matches the user-defined resolution in the settings save file.
+    /// </summary>
+    /// <param name="graphics">A reference to the MonoGame GraphicsDeviceManager, used to obtain the current resolution.</param>
+    /// <param name="settingsSaveData">A reference to the SettingsSaveData class, used to obtain the intended resolution.</param>
     public static void AssertApplicationResolution(GraphicsDeviceManager graphics, SettingsSaveData settingsSaveData)
     {
         var actualDisplayWidth = graphics.PreferredBackBufferWidth;
@@ -50,6 +72,11 @@ public static class TestRunner
         }
     }
 
+    /// <summary>
+    /// A method to test if the application is fullscreen or windowed, and if that matches what is noted in the settings save file.
+    /// </summary>
+    /// <param name="graphics">A reference to the MonoGame GraphicsDeviceManager, used to obtain the current display mode.</param>
+    /// <param name="settingsSaveData">A reference to the SettingsSaveData class, used to obtain the intended display mode.</param>
     public static void AssertApplicationDisplayMode(GraphicsDeviceManager graphics, SettingsSaveData settingsSaveData)
     {
         var actualDisplayMode = graphics.IsFullScreen;
@@ -69,6 +96,11 @@ public static class TestRunner
         }
     }
 
+    /// <summary>
+    /// A test to check if the number of lesson simulations loaded is as expected.
+    /// </summary>
+    /// <param name="listView">The widget displaying the available lesson simulations.</param>
+    /// <param name="folderPath">The folder in which the lesson simulations are located.</param>
     public static void AssertLessonsLoaded(ListView listView, string folderPath)
     {
         var numOfWidgets = listView.Widgets.Count;
@@ -97,6 +129,11 @@ public static class TestRunner
         }
     }
     
+    /// <summary>
+    /// A test to check if the number of lesson sandbox loaded is as expected.
+    /// </summary>
+    /// <param name="listView">The widget displaying the available sandbox simulations.</param>
+    /// <param name="folderPath">The folder in which the sandbox simulations are located.</param>
     public static void AssertUserSimsLoaded(ListView listView, string folderPath)
     {
         var numOfWidgets = listView.Widgets.Count;
@@ -125,6 +162,11 @@ public static class TestRunner
         }
     }
 
+    /// <summary>
+    /// A method to test if the file has been renamed.
+    /// </summary>
+    /// <param name="oldPath">The path where the file was located.</param>
+    /// <param name="newPath">The new path of the file.</param>
     public static void AssertFileRename(string oldPath, string newPath)
     {
         var oldPathExists = File.Exists(oldPath);
@@ -149,6 +191,10 @@ public static class TestRunner
         }
     }
 
+    /// <summary>
+    /// A method to test if the file has been deleted.
+    /// </summary>
+    /// <param name="filePath">The path where the deleted was prior to deletion.</param>
     public static void AssertFileDeletion(string filePath)
     {
         if (File.Exists(filePath))
@@ -165,6 +211,11 @@ public static class TestRunner
         }
     }
 
+    /// <summary>
+    /// A method to test if the bodies have been loaded correctly.
+    /// </summary>
+    /// <param name="bodies">The bodies within into the simulation.</param>
+    /// <param name="bodiesData">The bodies stored in the save file, used for the sake of comparison.</param>
     public static void AssertBodiesDataIntegrity(List<Body> bodies, List<BodyData> bodiesData)
     {
         var numOfInstanceBodies = bodies.Count;
@@ -184,6 +235,11 @@ public static class TestRunner
         }
     }
 
+    /// <summary>
+    /// A method to test if the lesson content has been loaded correctly.
+    /// </summary>
+    /// <param name="lessonPages">The instantiated list of lesson content.</param>
+    /// <param name="lessonData">The lesson contented as stored in its save file.</param>
     public static void AssertLessonDataIntegrity(List<LessonPage> lessonPages, List<LessonPage> lessonData)
     {
         var numOfInstanceLessons = lessonPages.Count;
@@ -203,6 +259,11 @@ public static class TestRunner
         }
     }
 
+    /// <summary>
+    /// A method to test if the new body has been created.
+    /// </summary>
+    /// <param name="bodies">The bodies within the simulation.</param>
+    /// <param name="newBody">The new body that has been created, used for comparison.</param>
     public static void AssertBodyCreated(List<Body> bodies, Body newBody)
     {
         if (bodies.Contains(newBody))
@@ -219,6 +280,11 @@ public static class TestRunner
         }
     }
 
+    /// <summary>
+    /// A method to test if a body has been edited.
+    /// </summary>
+    /// <param name="bodies">The bodies within the simulation.</param>
+    /// <param name="editedBody">The body that has been edited, used for comparison.</param>
     public static void AssertBodyEdited(List<Body> bodies, Body editedBody)
     {
         if (bodies.Contains(editedBody))
@@ -235,6 +301,11 @@ public static class TestRunner
         }
     }
 
+    /// <summary>
+    /// A method to test if a body has been deleted.
+    /// </summary>
+    /// <param name="bodies">The bodies within the simulation.</param>
+    /// <param name="deletedBody">The body that has been deleted, used for comparison.</param>
     public static void AssertBodyDeleted(List<Body> bodies, Body deletedBody)
     {
         if (!bodies.Contains(deletedBody))
@@ -251,6 +322,12 @@ public static class TestRunner
         }
     }
 
+    /// <summary>
+    /// A helper method used for the simulation save test, with it checking if the length instantiated data matches the saved data.
+    /// </summary>
+    /// <param name="instanceData">The instantiated SimulationSaveData object.</param>
+    /// <param name="saveData">The data as read directly from the save file.</param>
+    /// <returns></returns>
     private static bool CompareSaveData(SimulationSaveData instanceData, SimulationSaveData saveData)
     {
         var bodies = instanceData.Bodies.Count == saveData.Bodies.Count;
@@ -259,6 +336,12 @@ public static class TestRunner
         return bodies && pages;
     }
 
+    /// <summary>
+    /// A method to test if a simulation has been correctly saved.
+    /// </summary>
+    /// <param name="filePath">The path where the save file is located.</param>
+    /// <param name="instanceData">The currently instantiated simulation data.</param>
+    /// <param name="saveSystem">A reference to a SaveSystem class.</param>
     public static void AssertSimulationSaved(string filePath, SimulationSaveData instanceData, SaveSystem saveSystem)
     {
         var saveData = saveSystem.LoadSimulation(filePath);
@@ -279,6 +362,9 @@ public static class TestRunner
         _testingComplete = true;
     }
 
+    /// <summary>
+    /// A method to save the results of testing to a .txt file, only called when _testingComplete is set to true.
+    /// </summary>
     public static void SaveResults()
     {
         if (!_testingComplete) return;
